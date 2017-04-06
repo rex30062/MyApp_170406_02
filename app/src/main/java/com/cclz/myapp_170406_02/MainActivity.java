749dbb27873863a5fe0732a8fc72352e3815f148
Cclz <rex620620>
2017-04-06 11:49:40 +0800

    RecyclerView 2

--- app/src/main/java/com/cclz/myapp_170406_02/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170406_02/MainActivity.java
@@ -3,6 +3,7 @@ package com.cclz.myapp_170406_02;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +11,18 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView.Adapter<MyAdapter.ViewHolder> mAdapter;
     RecyclerView.LayoutManager mLayoutManager;
     private Context context;
+    String data[]={"AAA", "BBBB", "CC", "DDDDD", "E"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context=this;
+        mRecyclerView=(RecyclerView)findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager=new LinearLayoutManager(context);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter=new MyAdapter(context, data);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 }
--- app/src/main/java/com/cclz/myapp_170406_02/MyAdapter.java
+++ app/src/main/java/com/cclz/myapp_170406_02/MyAdapter.java
@@ -1,32 +1,44 @@
 package com.cclz.myapp_170406_02;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by YVTC on 2017/4/6.
  */
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    String data[];
+    Context context;
+    public MyAdapter(Context context, String[] data){
+        this.data=data;
+        this.context=context;
+    }
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.my_text_view, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder.tv.setText(data[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.length;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tv;
         public ViewHolder(View itemView) {
             super(itemView);
+            tv=(TextView)itemView.findViewById(R.id.textView);
         }
     }
 }
